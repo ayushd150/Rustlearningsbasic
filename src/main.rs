1f809d6ad7781d9c2c5605b8fb8620d4f34d2106
Ayush Das <23f2001277@ds.study.iitm.ac.in>
2025-06-07 17:19:56 +0530

    initial changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+// fn main() {
+//     println!("Hello, world!");
+// }
 
+fn main(){
+    let x: i32 = -5; //both positive and negative
+    let y: u32 = 1000; // only positive
+    let z: f32 = 12.2222; //both positive and negative and have decimal places
+    print!("x:{}",x);
+    print!("y:{}",y);
+    print!("z:{}",z);
+}//mutate-change value 
